7bf51212bb536d5dddca2cfaeaaadcc991363bfc
Job Muturi <muirimuturi17@gmail.com>
2023-03-15 20:45:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+} 
+saturdayFun ();
+saturdayFun ("bathe my dog");
+
+function mondayWork (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+mondayWork ();
+
+function wrapAdjective (wrap = "*") {
+  return function (adjective = "special") {
+    return `You are ${wrap}${adjective}${wrap}!`; 
+    }
+   
+}
